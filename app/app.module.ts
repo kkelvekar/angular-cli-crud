faa42e0c831989ab8bdd25faa0f710d5b103b831
Kaustubh <kkelvekar.5@hotmail.com>
2017-05-27 16:02:17 +0530

    Added feature modules

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,28 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
-import { HttpModule } from "@angular/http"
-import { RouterModule } from "@angular/router"
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { ProductDetailGuard } from './product/product-guard.service';
+import { AppComponent }  from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductListComponent } from './product/product-list.component';
-import { ProductDetailComponent } from './product/product-detail.component';
-import { ProductFilterPipe } from './product/product-filter.pipe';
-import { StarComponent } from './shared/star.component';
+
+/* Feature Modules */
+import { ProductModule } from './product/product.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpModule,
+  imports: [
+    BrowserModule,
+    HttpModule,
     RouterModule.forRoot([
-      {path:"products",component: ProductListComponent},
-      {path:"product/:id",canActivate:[ProductDetailGuard],component: ProductDetailComponent},
-      {path:"welcome",component: WelcomeComponent},
-      {path:"",redirectTo:"welcome",pathMatch:"full"}   
-    ])
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductModule
   ],
-  declarations: [AppComponent, WelcomeComponent,ProductListComponent, ProductDetailComponent ,ProductFilterPipe, StarComponent],
-  providers:[ProductDetailGuard],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    WelcomeComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
